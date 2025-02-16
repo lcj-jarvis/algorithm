67cbc59086d4df79c443b6b8c32689d1cb0f45cb
lu <1802772962@qq.com>
2025-02-16 17:16:01 +0800

    fix: ListNode属性和方法改成public

--- src/main/java/com/mrlu/base/datastruct/linkedlist/ListNode.java
+++ src/main/java/com/mrlu/base/datastruct/linkedlist/ListNode.java
@@ -8,12 +8,18 @@ package com.mrlu.base.datastruct.linkedlist;
  */
 public class ListNode {
 
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      public int val;
+      public ListNode next;
+      public ListNode() {}
+      public ListNode(int val) { this.val = val; }
+      public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                '}';
+    }
 
     /**
      * 删除倒数第n个节点
